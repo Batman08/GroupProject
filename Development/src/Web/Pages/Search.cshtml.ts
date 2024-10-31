62f8733ca73268ba816ec5640ca2fa16ccd9c799
bilal asghar <asgharbilal8@gmail.com>
2024-10-31 12:17:10 +0000

    01 - change callback function name

--- Development/src/Web/Pages/Search.cshtml.ts
+++ Development/src/Web/Pages/Search.cshtml.ts
@@ -6,8 +6,8 @@ class Search {
 
 
     /* Controls */
-    private readonly _divSearchContainer = document.getElementById("divSearchContainer") as HTMLDivElement;
-    private readonly _formModuleSearch = this._divSearchContainer.querySelector("#formModuleSearch") as HTMLFormElement;
+    private readonly _container = document.getElementById("divSearchContainer") as HTMLDivElement;
+    private readonly formModuleSearch = this._container.querySelector("#formModuleSearch") as HTMLFormElement;
 
 
     /* Init */
@@ -21,32 +21,15 @@ class Search {
 
 
     /* GetModuleOptions */
-    private ServerRequest_GetModuleOptions3(ev: SubmitEvent): void {
-        ev.preventDefault();
-
-        const formData: FormData = new FormData(this._formModuleSearch);
-
-        const dataToServer: ModuleSearchDTO = {
-            ModuleSearch: formData.get("ModuleSearch").toString()
-        };
-
-        const xhr = new XMLHttpRequest();
-        xhr.open('POST', this._urlGetModuleOptions);
-        xhr.setRequestHeader("XSRF-TOKEN", Utilities.GetVerficationToken(this._formModuleSearch));
-        xhr.setRequestHeader('Content-type', 'application/json');
-        xhr.onload = () => xhr.status === 200 ? this.ServerRequestResponse_GetModuleOptions(JSON.parse(xhr.response)) : console.log(xhr);
-        xhr.send(JSON.stringify(dataToServer));
-    }
-
     private async ServerRequest_GetModuleOptions(ev: SubmitEvent): Promise<void> {
         ev.preventDefault();
 
-        const formData: FormData = new FormData(this._formModuleSearch);
+        const formData: FormData = new FormData(this.formModuleSearch);
         const dataToServer: ModuleSearchDTO = { ModuleSearch: formData.get("ModuleSearch").toString() };
         const response: Response = await fetch(this._urlGetModuleOptions, {
             method: 'POST',
             headers: {
-                'XSRF-TOKEN': Utilities.GetVerficationToken(this._formModuleSearch),
+                'XSRF-TOKEN': Utilities.GetVerficationToken(this.formModuleSearch),
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(dataToServer)
@@ -54,18 +37,18 @@ class Search {
 
         if (response.ok) {
             const dataFromServer = await response.json();
-            this.ServerRequestResponse_GetModuleOptions(dataFromServer);
+            this.ServerRequestDone_GetModuleOptions(dataFromServer);
         } else {
             const error = await response.text();
             console.error(`Error ${response.status} ${response.statusText}:`, error);
         }
     }
 
-    private ServerRequestResponse_GetModuleOptions(dataFromServer: any): void {
+    private ServerRequestDone_GetModuleOptions(dataFromServer: any): void {
         console.log(dataFromServer);
     }
 
     private BindSubmit_GetModuleOptions(): void {
-        this._formModuleSearch.onsubmit = (ev: SubmitEvent) => this.ServerRequest_GetModuleOptions(ev);
+        this.formModuleSearch.onsubmit = (ev: SubmitEvent) => this.ServerRequest_GetModuleOptions(ev);
     }
 }
