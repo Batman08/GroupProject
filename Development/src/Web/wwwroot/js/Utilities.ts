090bf003fc3998ab356926664480751324396ee2
bilal asghar <asgharbilal8@gmail.com>
2024-11-11 21:32:02 +0000

    01 - add utility function remove url query string

--- Development/src/Web/wwwroot/js/Utilities.ts
+++ Development/src/Web/wwwroot/js/Utilities.ts
@@ -7,4 +7,10 @@
         const token = formEl.querySelector("input[name=__RequestVerificationToken]") as HTMLInputElement;
         return token ? token.value : "";
     }
+    
+    public static RemoveUrlQueryString(): void {
+        const currentUrl: string = window.location.href; //Get the current URL
+        const urlWithoutQueryString: string = currentUrl.split('?')[0]; // Remove the query string
+        window.history.replaceState({}, document.title, urlWithoutQueryString); // Change the URL without refreshing the page
+    }
 }
