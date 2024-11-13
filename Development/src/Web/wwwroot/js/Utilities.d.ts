0dc149e89f3276e6b459c424c84ba6b74b7105ea
bilal asghar <asgharbilal8@gmail.com>
2024-11-13 15:45:30 +0000

    01 - add alerts utility function

---
+++ Development/src/Web/wwwroot/js/Utilities.d.ts
@@ -0,0 +1,8 @@
+﻿type CustomAlert = {
+    Message: string;
+    Format: "Default" | "WithAccentColour";
+    Type: "primary" | "secondary" | "success" | "danger" | "warning" | "info" | "light" | "dark";
+    AdditionalClasses?: string;
+    OverrideWidthToMax?: boolean;
+    IsDismissable?: boolean;
+}
