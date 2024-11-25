920eb1f3b352023c860bb83562228f38349defec
bilal asghar <asgharbilal8@gmail.com>
2024-11-25 23:57:36 +0000

    main - middle module now shows after beginning module

--- Development/src/Domain/Features/StoryModule/StoryModuleDTO.cs
+++ Development/src/Domain/Features/StoryModule/StoryModuleDTO.cs
@@ -5,5 +5,9 @@
         public int ModuleId { get; set; }
         public string Name { get; set; } = "";
         public string Contents { get; set; } = "";
+        public string PassChoiceText { get; set; } = "";
+        public string PassChoiceResult { get; set; } = "";
+        public string FailChoiceText { get; set; } = "";
+        public string FailChoiceResult { get; set; } = "";
     }
 }
